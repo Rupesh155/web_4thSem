03825e668a228d5e3ce883ab242a57bb177c55e0
Rupesh155 <rudhg35@gmail.com>
2026-06-12 12:54:35 +0530

    imp

--- lect14/my-react-app/src/NavBar.jsx
+++ lect14/my-react-app/src/NavBar.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
+
 // npm i react-router-dom
 const NavBar = () => {
   return (
     <div>
         <nav>
-            <Link to={"/"}>   
+         
             <li>Home</li>
-            </Link>
-            <Link to={'/about'}>  
+        
+     
             <li>About</li>
-            </Link>
-            <Link   to={"/cart"}>  
+       
+          <Link to={'/cart'}> 
             <li>Cart</li>
             </Link>
+          
 
         </nav>
     </div>
